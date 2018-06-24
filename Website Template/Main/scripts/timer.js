955234576f384e58fb5b612fc745474dccd030ef
jena satkunarajan <st116472@stud.uni-stuttgart.de>
2018-06-24 19:22:40 +0200

    restructured some folders, addes overhauled templates

---
+++ Website Template/Main/scripts/timer.js
@@ -0,0 +1,18 @@
+var currentTime = new Date();
+currentTime.setMinutes(currentTime.getMinutes() + 1);
+var x = setInterval(function () {
+
+    var now = new Date().getTime();
+
+    var distance = currentTime.getTime() - now;
+
+    //var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    $("#timer").text("Timer: " + seconds + "s ");
+
+    if (distance <= 0) {
+        clearInterval(x);
+        $("#timer").text("EXPIRED");
+    }
+}, 1000);
