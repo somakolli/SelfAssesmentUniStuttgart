e420fb8bbe7898bda5f8ace530b21a303b3b4350
Rezarak <34161762+Rezarak@users.noreply.github.com>
2018-07-09 17:29:46 +0200

    Added MediaPath support
    //Todo: XML-Integration, Overviewing & Editing Questions, Cells Uneditble

--- src/src/main/java/creator/Tabelle.java
+++ src/src/main/java/creator/Tabelle.java
@@ -27,6 +27,7 @@ public class Tabelle {
 	// Saves Table contents in Questions as Java Objects
 	public ArrayList<Question> Questions = new ArrayList<Question>();
 	public ArrayList<Answer> Answers = new ArrayList<Answer>();
+	public ArrayList<String> MediaPaths = new ArrayList<String>();
 
 	public Tabelle() {
 
@@ -38,6 +39,8 @@ public class Tabelle {
 		frame.setLayout(null);
 		frame.setResizable(false);
 		frame.getContentPane().setBackground(Color.DARK_GRAY);
+		// getClass();
+		// frame.setIconImage();
 		JTable table = new JTable();
 
 		// create a table model and set a Column Identifiers to this model
@@ -94,60 +97,45 @@ public class Tabelle {
 		TimeField.setBounds(210, 270, 90, 40);
 		frame.add(TimeField);
 
-		// JTextField AnswerField = new JTextField("This is an Answer!");
-		// AnswerField.setBounds(400, 220, 200, 40);
-		// frame.add(AnswerField);
-
 		JTextField PointsField = new JTextField("10");
 		PointsField.setBounds(100, 270, 90, 40);
 		frame.add(PointsField);
-
-		// JTextField MediaPathField = new JTextField("C:/Users/...");
-		// MediaPathField.setBounds(400, 270, 200, 40);
-		// MediaPathField.addActionListener(new ALMediaChoosePath(MediaPathField));
-		// frame.add(MediaPathField);
 
 		// create JButtons
 		JButton btnAdd = new JButton("Add");
 		btnAdd.setBounds(750, 220, 100, 25);
 		btnAdd.setBackground(Color.GRAY);
 		btnAdd.setForeground(Color.WHITE);
-		
+
 		JButton btnDelete = new JButton("Delete");
 		btnDelete.setBounds(750, 265, 100, 25);
 		btnDelete.setBackground(Color.GRAY);
 		btnDelete.setForeground(Color.WHITE);
-		
+
 		JButton btnImportXML = new JButton("Import XML");
 		btnImportXML.setBounds(750, 310, 100, 25);
 		btnImportXML.setBackground(Color.GRAY);
 		btnImportXML.setForeground(Color.WHITE);
-		
+
 		JButton btnExportXML = new JButton("Export XML");
 		btnExportXML.setBounds(750, 355, 100, 25);
 		btnExportXML.setBackground(Color.GRAY);
 		btnExportXML.setForeground(Color.WHITE);
-		
+
 		JButton btnUpdate = new JButton("Update");
 		btnUpdate.setBounds(750, 400, 100, 25);
 		btnUpdate.setBackground(Color.GRAY);
 		btnUpdate.setForeground(Color.WHITE);
-		
+
 		JButton btnAddAnswers = new JButton("Add Answers");
 		btnAddAnswers.setBounds(400, 220, 200, 40);
 		btnAddAnswers.setBackground(Color.GRAY);
 		btnAddAnswers.setForeground(Color.WHITE);
-		
+
 		JButton btnAddMediaPaths = new JButton("Add Media Paths");
 		btnAddMediaPaths.setBounds(400, 270, 200, 40);
 		btnAddMediaPaths.setBackground(Color.GRAY);
 		btnAddMediaPaths.setForeground(Color.WHITE);
-
-		// create JScrollPane
-		JScrollPane pane = new JScrollPane(table);
-		pane.setBounds(5, 5, 870, 200);
-
-		frame.add(pane);
 
 		// add JButtons to the jframe
 		frame.add(btnAdd);
@@ -156,6 +144,11 @@ public class Tabelle {
 		frame.add(btnImportXML);
 		frame.add(btnAddAnswers);
 		frame.add(btnAddMediaPaths);
+
+		// create JScrollPane
+		JScrollPane pane = new JScrollPane(table);
+		pane.setBounds(5, 5, 870, 200);
+		frame.add(pane);
 
 		// create an array of objects to set the row data
 		Object[] row = new Object[6];
@@ -166,14 +159,13 @@ public class Tabelle {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 
-				// add Question to the List
+				// create new question object and add it to the list
 				Question newq = new Question();
 				newq.setAnswers(Answers);
+				newq.setMediaPaths(MediaPaths);
 				newq.setQuestion(QuestionField.getText());
 				newq.setPoints(Integer.parseInt(PointsField.getText()));
 				newq.setTime(Integer.parseInt(TimeField.getText()));
-				
-
 				Questions.add(newq);
 
 				// write into rows
@@ -182,11 +174,16 @@ public class Tabelle {
 				row[2] = Answers.size();
 				row[3] = PointsField.getText();
 				row[4] = TimeField.getText();
-				// row[5] = MediaPathField.getText();
+				row[5] = MediaPaths.size();
+
+				// remove saved Answers and MediaPaths
+				MediaPaths.removeAll(MediaPaths);
+				Answers.removeAll(Answers);
 
 				// add row to the model
-				Answers.removeAll(Answers);
 				model.addRow(row);
+
+				System.out.println(Questions);
 			}
 		});
 
@@ -324,6 +321,46 @@ public class Tabelle {
 				Answer8CB.setBackground(Color.DARK_GRAY);
 				frame.add(Answer8CB);
 
+				// Rewrite already Saved Answers
+
+				for (int i = 0; i < Answers.size(); i++) {
+					if (i == 0) {
+						Answer1Field.setText(Answers.get(i).getContent());
+						Answer1CB.setSelected(Answers.get(i).getCorrect());
+					}
+					if (i == 1) {
+						Answer2Field.setText(Answers.get(i).getContent());
+						Answer2CB.setSelected(Answers.get(i).getCorrect());
+					}
+					if (i == 2) {
+						Answer3Field.setText(Answers.get(i).getContent());
+						Answer3CB.setSelected(Answers.get(i).getCorrect());
+					}
+					if (i == 3) {
+						Answer4Field.setText(Answers.get(i).getContent());
+						Answer4CB.setSelected(Answers.get(i).getCorrect());
+					}
+					if (i == 4) {
+						Answer5Field.setText(Answers.get(i).getContent());
+						Answer5CB.setSelected(Answers.get(i).getCorrect());
+					}
+					if (i == 5) {
+						Answer6Field.setText(Answers.get(i).getContent());
+						Answer6CB.setSelected(Answers.get(i).getCorrect());
+					}
+					if (i == 6) {
+						Answer7Field.setText(Answers.get(i).getContent());
+						Answer7CB.setSelected(Answers.get(i).getCorrect());
+					}
+					if (i == 7) {
+						Answer8Field.setText(Answers.get(i).getContent());
+						Answer8CB.setSelected(Answers.get(i).getCorrect());
+					}
+
+				}
+				
+				Answers.removeAll(Answers);
+
 				JButton btnSave = new JButton("Save Answers");
 				btnSave.setBounds(500, 20, 200, 40);
 				btnSave.setBackground(Color.GRAY);
@@ -375,7 +412,7 @@ public class Tabelle {
 							Answer7.setCorrect(Answer7CB.isSelected());
 							Answers.add(Answer7);
 						}
-						if (!Answer8Field.getText().equals("") && !Answer1Field.getText().equals("First Answer")) {
+						if (!Answer8Field.getText().equals("") && !Answer8Field.getText().equals("Eigth Answer")) {
 							Answer Answer8 = new Answer();
 							Answer8.setContent(Answer8Field.getText());
 							Answer8.setCorrect(Answer8CB.isSelected());
@@ -384,10 +421,161 @@ public class Tabelle {
 						frame.dispose();
 					}
 				});
-				
+
 				frame.add(btnSave);
 				frame.setVisible(true);
 			}
+		});
+
+		// button Add Media Paths
+		btnAddMediaPaths.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// create JFrame and JTable
+				JFrame frame = new JFrame("Questions");
+				frame.setSize(800, 500);
+				frame.setLocationRelativeTo(null);
+				frame.setLayout(null);
+				frame.setResizable(false);
+				frame.getContentPane().setBackground(Color.DARK_GRAY);
+
+				JLabel check = new JLabel("Use the Buttons to select the path to your .jpg/.png file!");
+				check.setBounds(0, 0, 400, 20);
+				check.setForeground(Color.WHITE);
+				frame.add(check);
+
+				JTextField MP1Field = new JTextField("First media path");
+				MP1Field.setBounds(20, 20, 400, 40);
+				frame.add(MP1Field);
+				JButton MP1Button = new JButton("+");
+				MP1Button.setForeground(Color.WHITE);
+				MP1Button.setBounds(440, 20, 40, 40);
+				MP1Button.setBackground(Color.GRAY);
+				frame.add(MP1Button);
+
+				JTextField MP2Field = new JTextField("Second media path");
+				MP2Field.setBounds(20, 70, 400, 40);
+				frame.add(MP2Field);
+				JButton MP2Button = new JButton("+");
+				MP2Button.setForeground(Color.WHITE);
+				MP2Button.setBounds(440, 70, 40, 40);
+				MP2Button.setBackground(Color.GRAY);
+				frame.add(MP2Button);
+
+				JTextField MP3Field = new JTextField("Third media path");
+				MP3Field.setBounds(20, 120, 400, 40);
+				frame.add(MP3Field);
+				JButton MP3Button = new JButton("+");
+				MP3Button.setForeground(Color.WHITE);
+				MP3Button.setBounds(440, 120, 40, 40);
+				MP3Button.setBackground(Color.GRAY);
+				frame.add(MP3Button);
+
+				JTextField MP4Field = new JTextField("Fourth media path");
+				MP4Field.setBounds(20, 170, 400, 40);
+				frame.add(MP4Field);
+				JButton MP4Button = new JButton("+");
+				MP4Button.setForeground(Color.WHITE);
+				MP4Button.setBounds(440, 170, 40, 40);
+				MP4Button.setBackground(Color.GRAY);
+				frame.add(MP4Button);
+
+				JTextField MP5Field = new JTextField("Fifth media path");
+				MP5Field.setBounds(20, 220, 400, 40);
+				frame.add(MP5Field);
+				JButton MP5Button = new JButton("+");
+				MP5Button.setForeground(Color.WHITE);
+				MP5Button.setBounds(440, 220, 40, 40);
+				MP5Button.setBackground(Color.GRAY);
+				frame.add(MP5Button);
+
+				JTextField MP6Field = new JTextField("Sixth media path");
+				MP6Field.setBounds(20, 270, 400, 40);
+				frame.add(MP6Field);
+				JButton MP6Button = new JButton("+");
+				MP6Button.setForeground(Color.WHITE);
+				MP6Button.setBounds(440, 270, 40, 40);
+				MP6Button.setBackground(Color.GRAY);
+				frame.add(MP6Button);
+
+				JTextField MP7Field = new JTextField("Seventh media path");
+				MP7Field.setBounds(20, 320, 400, 40);
+				frame.add(MP7Field);
+				JButton MP7Button = new JButton("+");
+				MP7Button.setForeground(Color.WHITE);
+				MP7Button.setBounds(440, 320, 40, 40);
+				MP7Button.setBackground(Color.GRAY);
+				frame.add(MP7Button);
+
+				JTextField MP8Field = new JTextField("Eigth media path");
+				MP8Field.setBounds(20, 370, 400, 40);
+				frame.add(MP8Field);
+				JButton MP8Button = new JButton("+");
+				MP8Button.setForeground(Color.WHITE);
+				MP8Button.setBounds(440, 370, 40, 40);
+				MP8Button.setBackground(Color.GRAY);
+				frame.add(MP8Button);
+				
+				
+				for (int i = 0; i < MediaPaths.size(); i++) {
+					if (i == 0) {
+						MP1Field.setText(MediaPaths.get(i));
+					}
+					if (i == 1) {
+						MP2Field.setText(MediaPaths.get(i));
+					}
+					if (i == 2) {
+						MP3Field.setText(MediaPaths.get(i));
+					}
+					if (i == 3) {
+						MP4Field.setText(MediaPaths.get(i));
+					}
+					if (i == 4) {
+						MP5Field.setText(MediaPaths.get(i));
+					}
+					if (i == 5) {
+						MP6Field.setText(MediaPaths.get(i));
+					}
+					if (i == 6) {
+						MP7Field.setText(MediaPaths.get(i));
+					}
+					if (i == 7) {
+						MP8Field.setText(MediaPaths.get(i));
+					}
+
+				}
+				
+				MediaPaths.removeAll(MediaPaths);
+				
+
+				JButton btnSave = new JButton("Save media paths");
+				btnSave.setBounds(500, 20, 200, 40);
+				btnSave.setBackground(Color.GRAY);
+				btnSave.setForeground(Color.WHITE);
+				btnSave.addActionListener(new ActionListener() {
+					
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+
+						MediaPaths.add(MP1Field.getText());
+						MediaPaths.add(MP2Field.getText());
+						MediaPaths.add(MP3Field.getText());
+						MediaPaths.add(MP4Field.getText());
+						MediaPaths.add(MP5Field.getText());
+						MediaPaths.add(MP6Field.getText());
+						MediaPaths.add(MP7Field.getText());
+						MediaPaths.add(MP8Field.getText());
+
+						frame.dispose();
+					}
+				});
+
+				frame.add(btnSave);
+				frame.setVisible(true);
+			}
+
 		});
 
 		// // get selected row data From table to textfields
@@ -436,6 +624,7 @@ public class Tabelle {
 
 		Tabelle gui = new Tabelle();
 		System.out.println(gui.Questions);
+
 	}
 
 }
