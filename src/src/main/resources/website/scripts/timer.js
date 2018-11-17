1193e7967a3d16688352ec6a15a057a92f552561
Sokol Makolli <st116214@stud.uni-stuttgart.de>
2018-11-17 16:07:24 +0100

    fix timer

--- src/src/main/resources/website/scripts/timer.js
+++ src/src/main/resources/website/scripts/timer.js
@@ -4,22 +4,14 @@
  */
 function startTimer(limit) {
     $("#timer").css("visibility", "visible");
-    var currentTime = new Date();
-    currentTime.setMinutes(currentTime.getMinutes() + 1);
     var x = setInterval(function () {
-
-        var now = new Date().getTime();
-
-        var distance = currentTime.getTime() - now;
-
-        //var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * limit)) / 1000);
+        limit--;
 
         $("#timer").text("Timer: " + seconds + "s ");
         $(".bottom-right").click(function(){
             clearInterval(x);
         });
-        if (distance <= 0) {
+        if (limit <= 0) {
             clearInterval(x);
             $("#timer").text("EXPIRED");
             updateState();
